bcd9102705c40405b3b32d49d9ebdafe8b4b7ad5
nirali28cse <nirali28cse@gmail.com>
2017-01-08 18:47:54 +0530

    user email send 1

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=54.191.228.76;dbname=fuberme',
+    'dsn' => 'mysql:host=54.191.228.76;port=3306;dbname=fuberme',
     'username' => 'root',
     'password' => '12345',
     'charset' => 'utf8',
